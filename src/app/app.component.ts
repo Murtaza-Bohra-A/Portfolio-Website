df4710cf25a5773984548777bf63c2158b21bd43
Murtaza-Bohra-A <murtaza7249885185@gmail.com>
2025-10-11 16:35:37 +0530

    Impliment API calling and Validation using Reactive Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { RcontentComponent } from "./Components/rcontent/rcontent.component";
+import { HttpClientModule } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RcontentComponent],
+  imports: [RouterLink, RcontentComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
